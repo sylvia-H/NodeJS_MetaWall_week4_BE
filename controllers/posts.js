04166e9699419ed1833c262d0712d2c70119f9f5
sylvia-H <sylvie11gaia@gmail.com>
2022-06-02 22:37:55 +0800

    revise post.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,13 +6,18 @@ const User = require('../model/user');
 const PostController = {
   async getPosts(req, res) {
     // 貼文時間序列
-    const timeSort = req.query.timeSort == "asc" ? "createdAt":"-createdAt"
+    // const timeSort = req.query.timeSort == "asc" ? "createdAt":"-createdAt"
     // 搜尋貼文內容
-    const q = req.query.q !== undefined ? {"content": new RegExp(req.query.q)} : {};
-    const posts = await Post.find(q).populate({
+    // const q = req.query.q !== undefined ? {"content": new RegExp(req.query.q)} : {};
+    // const posts = await Post.find(q).populate({
+    //   path: 'author', 
+    //   select: 'name avatar',
+    // }).sort(timeSort);
+    
+    const posts = await Post.find().populate({
       path: 'author', 
       select: 'name avatar',
-    }).sort(timeSort);
+    });
     successHandler(res, posts);
   },
   async createPosts(req, res) {
