d0a83eb9cd1b54425f70be6f67a157a64be05199
sylvia-H <sylvie11gaia@gmail.com>
2022-06-02 22:30:04 +0800

    revise errors

--- model/post.js
+++ model/post.js
@@ -3,7 +3,7 @@ const postSchema = new mongoose.Schema(
   {
     author: {
       type: mongoose.Schema.ObjectId,
-      ref: "users",
+      ref: "user",
       required: [true, '未填寫貼文作者名'],
     },
     content: {
