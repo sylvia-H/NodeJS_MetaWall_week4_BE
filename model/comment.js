ba66d61e3a012ea6fc4f9c3f3901579fcede7bd8
sylvia-H <sylvie11gaia@gmail.com>
2022-06-03 17:01:49 +0800

    add comment model

--- model/comment.js
+++ model/comment.js
@@ -6,6 +6,7 @@ const commentSchema = new mongoose.Schema(
     },
     articleID: {
       type: mongoose.Schema.ObjectId,
+      ref: 'Post',
       required: [true, '沒有貼文 ID'],
     },
     author: {
